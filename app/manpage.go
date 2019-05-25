0c779b7109a756760b6aa93acc9f2061e1a72aa5
Πrmd <pirmd@users.noreply.github.com>
2019-05-25 18:19:03 +0200

    reformat comments

--- app/manpage.go
+++ app/manpage.go
@@ -13,13 +13,13 @@ import (
 	"github.com/pirmd/cli/style"
 )
 
-//ManSection identify the man section to which the command is belonging to
-//It should be most of the time 1
+//ManSection identify the man section to which the command is belonging to It
+//should be most of the time 1
 const ManSection = "1"
 
-//PrintManpage outputs to w the command's documentation in a manpage-like format.
-//It does not recurse over sub-commands if any, so that they are not fully documented
-//in this page and need to be generated separatly.
+//PrintManpage outputs to w the command's documentation in a manpage-like
+//format.  It does not recurse over sub-commands if any, so that they are not
+//fully documented in this page and need to be generated separatly.
 func PrintManpage(w io.Writer, c *command, st style.Styler) {
 	stMan := st.WithAutostyler(style.LightMarkup.Extend(style.Markup{
 		regexp.MustCompile(`\s((?:[^\s]+?)\([1-7]\))\s`):        style.FmtBold,
@@ -42,9 +42,8 @@ func PrintManpage(w io.Writer, c *command, st style.Styler) {
 	}
 }
 
-//GenerateManpage generates manpage for the given command.
-//It also recures over sub-commands - if any - to generate their
-//own manpages.
+//GenerateManpage generates manpage for the given command.  It also recures
+//over sub-commands - if any - to generate their own manpages.
 func GenerateManpage(c *command) error {
 	fname := fmtName(c) + "." + ManSection
 	f, err := os.Create(fname)
