22a9bc90e82d2c78850dd61f9ab3d449fbcdbc7c
Πrmd <pirmd@users.noreply.github.com>
2021-07-28 10:39:22 +0200

    Remove unnecessary error detection during ansi.Walk operation.

--- ansi/ansi.go
+++ ansi/ansi.go
@@ -11,14 +11,6 @@ var (
 	// ErrStopWalk is used as a return value from WalkFunc to indicate to Walk
 	// to interrupt its iteration.
 	ErrStopWalk = errors.New("stop walk")
-
-	// ErrIncompleteRune is the error raised if utf-8 decoding error occurs
-	// during write operation.
-	ErrIncompleteRune = errors.New("utf-8 decoding errors")
-
-	// ErrIncompleteAnsiEscSeq is the error raised if an incomplete ANSI
-	// sequence is encountered.
-	ErrIncompleteAnsiEscSeq = errors.New("incomplete ANSI escape sequence")
 )
 
 // WalkFunc is the type of the function that is called while going through a
@@ -27,12 +19,14 @@ var (
 // escape sequence or ANSI escape sequence per ANSI escape sequence in the
 // other case. If WalkFunc is called for an ANSI Escape Sequence, the curRune
 // will be -1.
+// Invalid rune's encoding ends up in curRune being of utf8.RuneError value.
 // If WalkFunc returns the ErrStopWalk error, Walk will interrupt the walk.
 type WalkFunc func(advance int, curRune rune, curSGREsc string) error
 
 // Walk walks through a slice of bytes that can contain ANSI escape codes and
 // run WalkFunc either on each rune that is not part of an escape sequence or
 // on each ANSI escape sequence.
+// Walk stops and returns any error raised by fn (that is not ErrStopWalk).
 func Walk(p []byte, fn WalkFunc) (err error) {
 	advance := 0
 	inEscSeq := false
@@ -40,10 +34,6 @@ func Walk(p []byte, fn WalkFunc) (err error) {
 
 	for len(p[advance:]) > 0 {
 		c, sz := utf8.DecodeRune(p[advance:])
-		if c == utf8.RuneError {
-			err = ErrIncompleteAnsiEscSeq
-			return
-		}
 		advance += sz
 
 		switch {
@@ -72,8 +62,9 @@ func Walk(p []byte, fn WalkFunc) (err error) {
 		}
 	}
 
+	// Incomplete or non-recognized escape sequence
 	if curEsc.Len() > 0 {
-		err = ErrIncompleteAnsiEscSeq
+		err = fn(advance, -1, curEsc.String())
 	}
 
 	return
